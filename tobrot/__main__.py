59b6e0755927ae96d2b242ef573157265850821d
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-08-29 11:53:58 +0700

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -195,17 +195,6 @@ if __name__ == "__main__":
     )
     app.add_handler(help_text_handler)
     #
-    new_join_handler = MessageHandler(
-        new_join_f, filters=~filters.chat(chats=AUTH_CHANNEL)
-    )
-    app.add_handler(new_join_handler)
-    #
-    group_new_join_handler = MessageHandler(
-        help_message_f,
-        filters=filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members,
-    )
-    app.add_handler(group_new_join_handler)
-    #
     call_back_button_handler = CallbackQueryHandler(button)
     app.add_handler(call_back_button_handler)
     #
